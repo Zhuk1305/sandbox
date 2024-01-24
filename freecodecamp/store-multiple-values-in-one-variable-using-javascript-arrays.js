83b735f9b81d8f4c0b8387ef893c57dc84a1b3d1
Zhuk1305 <vaneczhuk@gmail.com>
2024-01-24 17:11:54 +0100

    array

---
+++ freecodecamp/store-multiple-values-in-one-variable-using-javascript-arrays.js
@@ -0,0 +1,6 @@
+// Modify the new array myArray so that it contains both a string and a number (in that order).
+
+// Only change code below this line
+// const myArray = [];
+
+const myArray = ["Dog", 42];
