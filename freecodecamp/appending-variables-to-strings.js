008ba47f6901f39dee71ed128c1755fe61632cc2
Zhuk1305 <vaneczhuk@gmail.com>
2024-01-24 16:53:23 +0100

    appending string

---
+++ freecodecamp/appending-variables-to-strings.js
@@ -0,0 +1,7 @@
+// Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+// const someAdjective = "";
+// let myStr = "Learning to code is ";
+// Change code below this line
+const someAdjective = "a 42";
+let myStr = "Learning to code is ";
+myStr += someAdjective
