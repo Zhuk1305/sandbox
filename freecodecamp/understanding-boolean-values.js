c37e3069e330c7eb4a6ae9180c9b157f1e0b6f89
Zhuk1305 <vaneczhuk@gmail.com>
2024-01-28 18:15:02 +0100

    boolean values

---
+++ freecodecamp/understanding-boolean-values.js
@@ -0,0 +1,17 @@
+// Modify the welcomeToBooleans function so that it returns true instead of false.
+
+// function welcomeToBooleans() {
+//   // Only change code below this line
+
+//   return false; // Change this line
+
+//   // Only change code above this line
+// }
+
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
