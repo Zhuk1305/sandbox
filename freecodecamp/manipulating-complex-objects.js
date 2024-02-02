c27717bf34467511e625813bd1a6864974e9735d
Zhuk1305 <vaneczhuk@gmail.com>
2024-02-02 23:53:33 +0100

    manipulating complex object

---
+++ freecodecamp/manipulating-complex-objects.js
@@ -0,0 +1,42 @@
+// Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings
+
+
+// const myMusic = [
+//   {
+//     "artist": "Billy Joel",
+//     "title": "Piano Man",
+//     "release_year": 1973,
+//     "formats": [
+//       "CD",
+//       "8T",
+//       "LP"
+//     ],
+//     "gold": true
+//   }
+// ];
+
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Eminem",
+    "title": "Lose yourself",
+    "release_year": 202,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
